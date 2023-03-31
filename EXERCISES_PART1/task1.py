f5216bdc2c28c47ea470bc12b1288efc35cbf2dc
KaioOliveiraDomingues <KaioOliveiraDomingues1@gmail.com>
2023-03-31 19:44:40 -0300

    new files

---
+++ EXERCISES_PART1/task1.py
@@ -0,0 +1,3 @@
+n1 = input('N1 = ')
+n2 = input('N2 = ')
+print(n1,'+',n2,'=', int(n1)+int(n2))
---
+++ EXERCISES_PART1/task2_dice1.py
@@ -0,0 +1,9 @@
+import random
+import time
+
+print('DICE ROLL!!')
+time.sleep(2)
+print('Rolling...')
+time.sleep(5)
+dice = random.randint(1,6)
+print('DICE ROLLED: ', dice)
