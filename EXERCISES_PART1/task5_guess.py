9a610614bcbca3a8fd41d6d41a96280630d8c24b
KaioOliveiraDomingues <KaioOliveiraDomingues1@gmail.com>
2023-04-01 01:27:21 -0300

    Finished task5

--- EXERCISES_PART1/task5_guess.py
+++ EXERCISES_PART1/task5_guess.py
@@ -1,1 +1,16 @@
 #Adivinhação: Crie um programa que escolha um número aleatório entre 1 e 100 e permita que o usuário adivinhe qual é esse número.
+import random
+
+number = random.randint(1,100)
+guess = 0
+
+while number != guess:
+
+    guess = int(input('your guess:'))
+    if guess == number:
+        print('CORRECT, you guessed: ', guess, 'and it was: ', number)
+        break
+    elif guess > number:
+        print('wrong! the number is lower')
+    elif guess < number:
+        print('wrong! the number is higher')
